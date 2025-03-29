75c85dcf8066e7f6bd6e636487ebc055593ebdc9
nengren <54527708+nengren@users.noreply.github.com>
2025-03-29 22:16:05 +0800

    2

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',  // 静态导出
   images: {
+    unoptimized: true,  // Cloudflare Pages不支持Next.js的图像优化
     domains: ['images.unsplash.com', 'joy-grid.com'],
   },
+  trailingSlash: true,  // 确保URL以斜杠结尾
   i18n: {
     locales: ['zh', 'en'],
     defaultLocale: 'zh',
